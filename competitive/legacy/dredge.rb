97c899f98f47e256d58d3bf8463885016574ba1e
Anthony Chang <anthony.sh.chang@gmail.com>
2018-04-28 17:52:53 +0200

    WIP

---
+++ competitive/legacy/dredge.rb
@@ -0,0 +1,31 @@
+Creatures - 25
+4 Putrid Imp
+4 Narcomoeba
+3 Golgari Thug
+4 Stinkweed Imp
+4 Ichorid
+1 Flame-Kin Zealot
+4 Golgari Grave-Troll
+1 Iona, Angel 
+
+Other Spells - 19 
+1 Darkblast
+4 Faithless Looting
+4 Cabal Therapy
+3 Breakthrough
+4 Bridge from Below
+3 Dread Return
+
+Land - 15
+3 Cephalid Coliseum
+4 City of Brass
+4 Mana Confluence
+4 Gemstone Mine
+
+Sideboard - 15
+4 Firestorm
+4 Leyline of the Void
+3 Nature's Claim
+1 Terastodon
+1 Ashen Rider
+1 Sire of Insanity
